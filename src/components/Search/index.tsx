f7e62f1afcafdd2126ccdc50cf6fecf4e9aa85d6
Ruan Pablo <ruan_goio10@hotmail.com>
2022-08-08 18:58:16 -0300

    Fix: input weight

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import {
     Container,
     Input,
+    Button,
 } from './styles';
 
 export function Search() {
     return (
         <Container>
             <Input placeholder="Search" />
+            <Button />
         </Container>
     );
 }
--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -7,15 +7,25 @@ export const Container = styled.View`
    height: 56px;
    background-color: transparent;
    padding: 0px 24px;
+   flex-direction: row;
+   justify-content: space-between;
 `;
 
 
 export const Input = styled.TextInput<TextInputProps>`
-    width: auto;
+    width: 80%;
     height: 56px;
     background-color: ${({theme}) => theme.colors.white};
     border-radius: 6px;
     border: 3px solid ${({theme}) => theme.colors.primary[400]};
     padding: 0px 24px;
     font-size: 18px;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width:56px;
+    height: 56px;
+    background-color: ${({theme}) => theme.colors.white};
+    border-radius: 6px;
+    border: 3px solid ${({theme}) => theme.colors.primary[400]};
 `;
